ee0b88fae1ca9ffee3ef399e3e95c0f8e72867aa
tett23 <tett23@gmail.com>
2014-04-06 21:16:16 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mizuaoi::Application.config.secret_key_base = '99147535c9fc162927e72392286d799283b148d2d7e4465aaec58a3045503372215b1b6c6461bf55d935549d2c216b87297eac39ad00f6776c5919a584c87d2e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mizuaoi::Application.config.session_store :cookie_store, key: '_mizuaoi_session'
