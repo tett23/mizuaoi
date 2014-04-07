0ff00c99af7217b176e67f500eea4ffdc84784ef
tett23 <tett23@gmail.com>
2014-04-07 23:43:13 +0900

    動画一覧の背景色を変更

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,7 +2,7 @@ module VideosHelper
   def video_class(video)
     add_classes = []
 
-    add_classes << (video.is_encoded ? 'encoded' : 'unencoded')
+    add_classes << (video.is_encoded ? 'bg-success' : 'bg-warning')
     add_classes << (video.is_watched ? 'watched' : 'unwatched')
 
     add_classes.join(' ')
