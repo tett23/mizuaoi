ba1b353ee6f90070bfc84ec1eaac77f0e559a4ac
tett23 <tett23@gmail.com>
2014-11-02 04:32:11 +0900

    TS未削除から削除できるようにした

--- app/controllers/ts_controller.rb
+++ app/controllers/ts_controller.rb
@@ -3,7 +3,11 @@ class TsController < ApplicationController
     identification_codes = Dir.entries(Mizuaoi::Application.config.ts_dir).map do |item|
       item.gsub(/(\d+)\-.+$/, '\1')
     end.uniq
-    @videos = Video.list(identification_code: identification_codes).page(params[:page] || 1)
+    video_ids = Video.list(identification_code: identification_codes).map do |video|
+      video.id
+    end
+    @logs = JobLog.list(:encode, video_id: video_ids).group(:video_id).page(params[:page] || 1)
+
     add_breadcrumbs('未削除TS一覧', :ts, :index)
   end
 
