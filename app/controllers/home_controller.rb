9ef754e3fe1697408868329f2b42bae763099e06
tett23 <tett23@gmail.com>
2015-05-06 09:32:37 +0900

    home/indexが開けていなかった

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
   def index
     @jobs = Job.list().limit(20)
     @videos = Video.list().limit(20)
+    video_ids = @jobs.map do |job|
+      job.video_id
+    end.compact
+    @job_videos = Video.find(video_ids).inject({}) do |h, video|
+      h[video.id] = video
+
+      h
+    end
     @logs = JobLog.list().limit(20)
   end
 end
